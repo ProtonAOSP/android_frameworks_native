e8905461913e32dc9fda3d7d408307b2a6166ef3
Danny Lin <danny@kdrag0n.dev>
2020-12-28 00:41:36 -0800

    blit first to fix pass 1 sampling
    Change-Id: I9c12f7dc02f88ff6de934e9e71ccc4d777eab0be

--- libs/renderengine/gl/filters/BlurFilter.cpp
+++ libs/renderengine/gl/filters/BlurFilter.cpp
@@ -215,6 +215,15 @@ status_t BlurFilter::prepare() {
 
     glActiveTexture(GL_TEXTURE0);
 
+    GLFramebuffer* firstBuf = mPassFbos[0];
+    mCompositionFbo.bindAsReadBuffer();
+    firstBuf->bindAsDrawBuffer();
+    glBlitFramebuffer(0, 0,
+                      mCompositionFbo.getBufferWidth(), mCompositionFbo.getBufferHeight(),
+                      0, 0,
+                      firstBuf->getBufferWidth(), firstBuf->getBufferHeight(),
+                      GL_COLOR_BUFFER_BIT, GL_LINEAR);
+
     ALOGI("SARU: prepare - initial dims %dx%d", mPassFbos[0]->getBufferWidth(), mPassFbos[0]->getBufferHeight());
 
     // Set up downsampling shader
@@ -230,7 +239,7 @@ status_t BlurFilter::prepare() {
         ATRACE_NAME("BlurFilter::renderDownsamplePass");
 
         // Skip FBO 0 to avoid unnecessary blit
-        read = (i == 0) ? &mCompositionFbo : mPassFbos[i];
+        read = mPassFbos[i];
         draw = mPassFbos[i + 1];
 
         renderPass(read, draw);
