c52f5fb4585c80a92779a854c16b46bfa221b640
Danny Lin <danny@kdrag0n.dev>
2021-01-03 02:38:34 -0800

    perf[dither]: const int 16x16 mono blue noise array in shader
    Change-Id: Iebbc1f697798d459c9776a76baf430d8a657ad41

--- libs/renderengine/gl/filters/BlurFilter.cpp
+++ libs/renderengine/gl/filters/BlurFilter.cpp
@@ -407,13 +407,38 @@ string BlurFilter::getMixFragShader() const {
         in highp vec2 vUV;
         out vec4 fragColor;
 
+        const int kBlueNoise[256] = int[](
+            111,  49, 142, 162, 113, 195,  71, 177, 201,  50, 151,  94,  66,  37,  85, 252,
+             25,  99, 239, 222,  32, 250, 148,  19,  38, 106, 220, 170, 194, 138,  13, 167,
+            125, 178,  79,  15,  65, 173, 123,  87, 213, 131, 247,  23, 116,  54, 229, 212,
+             41, 202, 152, 132, 189, 104,  53, 236, 161,  62,   1, 181,  77, 241, 147,  68,
+              2, 244,  56,  91, 230,   5, 204,  28, 187, 101, 144, 206,  33,  92, 190, 107,
+            223, 164, 114,  36, 214, 156, 139,  70, 245,  84, 226,  48, 126, 158,  17, 135,
+             83, 196,  21, 254,  76,  45, 179, 115,  12,  40, 169, 105, 253, 176, 211,  59,
+            100, 180, 145, 122, 172,  97, 235, 129, 215, 149, 199,   8,  72,  26, 238,  44,
+            232,  31,  69,  11, 205,  58,  18, 193,  88,  60, 112, 221, 140,  86, 120, 153,
+            208, 130, 243, 160, 224, 110,  34, 248, 165,  24, 234, 184,  52, 198, 171,   6,
+            108, 188,  51,  89, 137, 186, 154,  78,  47, 134,  98, 157,  35, 249,  95,  63,
+             16,  75, 219,  39,   0,  67, 228, 121, 197, 240,   3,  74, 127,  20, 227, 143,
+            246, 175, 119, 200, 251, 103, 146,  14, 209, 174, 109, 218, 192,  82, 203, 163,
+             29,  93, 150,  22, 166, 182,  55,  30,  90,  64,  42, 141, 168,  57, 117,  46,
+            216, 233,  61, 128,  81, 237, 217, 118, 159, 255, 185,  27, 242, 102,   4, 133,
+             73, 191,   9, 210,  43,  96,   7, 136, 231,  80,  10, 124, 225, 207, 155, 183
+        );
+
+        float getNoise() {
+            int x = int(mod(gl_FragCoord.x, 16.0));
+            int y = int(mod(gl_FragCoord.y, 16.0));
+            return float(kBlueNoise[x + y * 16]) / 255.0;
+        }
+
         void main() {
             vec4 blurred = texture(uBlurredTexture, vUV);
             vec4 composition = texture(uCompositionTexture, vUV);
 
             // First /64: screen coordinates -> texture coordinates (UV)
             // Second /64: reduce magnitude to make it a dither instead of an overlay (from Bayer 8x8)
-            vec3 noise = texture(uDitherTexture, gl_FragCoord.xy / 64.0).rgb;
+            vec3 noise = vec3(getNoise());
             // Normalize to signed [-0.5; 0.5] range and divide down to (+-)1/255
             // This minimizes visible noise as only a 1/255 step is required for 8-bit quantization
             vec3 dither = (noise - 0.5) / 255.0;
